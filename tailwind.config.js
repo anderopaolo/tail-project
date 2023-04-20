d3977a19aa6126031a95d4f014705a46eed9e0c1
sohailmahmoud17 <119149704+sohailmahmoud17@users.noreply.github.com>
2023-04-20 23:26:36 +0200

    upload the tailwind design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '600px',
+      md: '991px',
+      lg: '1140px',
+      xl: '1440px'
+    },
+    extend: {
+    }
+  },
+  plugins: [],
+}
